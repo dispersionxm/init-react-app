0848f28c9758f0ba2851f6149821f1c68679b712
Sancho <sanzarospanov@Sanchos-MacBook-Air.local>
2024-10-08 19:58:37 +0500

    React-components task1 ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,80 @@
 import { useState } from 'react'
-import ReactLogo from './assets/react.svg?react'
-import './App.css'
+import style from './App.module.scss'
 
-const thisYear = new Date().getFullYear()
+const setTime = () => {
+	const date = new Date()
+	const currentTime = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} ${date.toLocaleTimeString('uz-UZ')}`
+	return `${currentTime}`
+}
+
+console.log(setTime())
 
 export const App = () => {
-	const [count, setCount] = useState(0)
+	const [isValidValue, setIsValidValue] = useState(false)
+	const onInputButtonClick = () => {
+		const promptValue = prompt().trim()
+		if (promptValue.length > 3) {
+			setValue(promptValue)
+			setError('')
+			setIsValidValue(true)
+		} else {
+			setError(promptValue)
+			setIsValidValue(false)
+		}
+	}
+	const onAddButtonClick = () => {
+		setList(list => [
+			...list,
+			{
+				id: Date.now(),
+				value,
+			},
+		])
+		setIsListEmpty(false)
+	}
 
+	const [value, setValue] = useState()
+	const [error, setError] = useState()
+	const [list, setList] = useState([])
+	const [isListEmpty, setIsListEmpty] = useState(true)
 	return (
-		<>
-			<h1>Hello World!</h1>
-			<ReactLogo />
-			<h2>{thisYear}</h2>
-
-			<hr />
-		</>
+		<div className={style.app}>
+			<h1 className={style.pageHeading}>Ввод значения</h1>
+			<p className={style.noMarginText}>
+				Текущее значение <code>value</code>: &#34;
+				<output className={style.currentValue}>{value}</output>&#34;
+			</p>
+			<div className={error !== '' ? style.error : style.inactiveError}>
+				Введенное значение должно содержать минимум 3 символа
+			</div>
+			<div className={style.buttonsContainer}>
+				<button className={style.button} onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
+				<button
+					className={style.button}
+					disabled={!isValidValue}
+					onClick={onAddButtonClick}
+				>
+					Добавить в список
+				</button>
+			</div>
+			<div className={style.listContainer}>
+				<h2 className={style.listHeading}>Список:</h2>
+				<div
+					className={`${style.noMarginText} ${isListEmpty ? '' : style.disabledP}`}
+				>
+					Нет добавленных элементов
+				</div>
+				<ul className={style.list}>
+					{list.map(item => (
+						<li key={item.id}>
+							<strong>{item.value}</strong>{' '}
+							<span className={style.date}>{setTime()}</span>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
 	)
 }
